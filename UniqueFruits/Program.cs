60dec56b94c9c1875e7fae834bbeec258a3d15b5
Ahmed.Elfaham <43139524+AhmedElfaham@users.noreply.github.com>
2022-05-21 03:49:01 +0200

    Finalize Solution
    Finalize Solution

--- UniqueFruits/Program.cs
+++ UniqueFruits/Program.cs
@@ -10,6 +10,78 @@ namespace UniqueFruits
     {
         static void Main(string[] args)
         {
+            List<string> A1 = new List<string> { "Watermelon", "Apple", "Mango", "Guava", "Banana" };
+            List<string> A2 = new List<string> { "Orange", "Kiwi", "Apple", "Watermelon" };
+            List<string> R = new List<string>();
+
+            SelectdistinctElementFrom(ref A1, ref A2, ref R);
+            SelectdistinctElementFrom(ref A2, ref A1, ref R);
+            StringBuilder summery = new StringBuilder();
+            Console.Write("A1 Array is : ");
+            foreach (string elm in A1)
+            {
+                summery.Append(elm + " ,");
+
+            }
+            Console.WriteLine(summery.Remove(summery.Length - 1, 1).ToString());
+            summery.Clear();
+            Console.Write("A2 Array is : ");
+            foreach (string elm in A2)
+            {
+                summery.Append(elm + " ,");
+            }
+            Console.WriteLine(summery.Remove(summery.Length - 1, 1).ToString());
+            summery.Clear();
+            Console.WriteLine("---------------------------------------\n");
+            Console.Write("Result Array is : ");
+            foreach (string elm in R)
+            {
+                summery.Append(elm + " ,");
+            }
+
+            Console.WriteLine(summery.Remove(summery.Length - 1, 1).ToString());
+            summery.Clear();
+            Console.ReadLine();
+
+
+
+        }
+
+
+        public static void SelectdistinctElementFrom(ref List<string> A1, ref List<string> A2, ref List<string> R)
+        {
+            for (int i = 0; i < A1.Count; i++)
+            {
+                bool Isredundant = false;
+                for (int j = 0; j < A2.Count; j++)
+                {
+                    if (A1[i] == A2[j])
+                    {
+                        Isredundant = true;
+                        break;
+                    }
+                }
+
+                if (!Isredundant)
+                {
+                    bool hasAddedbefore = false;
+                    foreach (string elm in R)
+                    {
+                        if (elm == A1[i])
+                        {
+                            hasAddedbefore = true;
+                            break;
+                        }
+
+                    }
+
+                    if (!hasAddedbefore)
+                    {
+                        R.Add(A1[i]);
+                    }
+
+                }
+            }
         }
     }
 }
